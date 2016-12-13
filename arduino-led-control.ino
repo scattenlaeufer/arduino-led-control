a64f7f3a732ffaa252ae502e8d5d70cd7a907f63
Björn Guth <bjoern.guth@rwth-aachen.de>
2016-12-13 18:59:51 +0100

    first version of a working led control

--- arduino-led-control.ino
+++ arduino-led-control.ino
@@ -1,20 +1,37 @@
-//#include<FastLED/FastLED.h>
+#include <FastLED.h>
 
 #define ws2812b_pin 12
-#define n_led 30
+#define n_leds 150
+
+CRGB leds[n_leds];
 
 void setup() {
-	pinMode(ws2812b_pin, OUTPUT);
+	//pinMode(ws2812b_pin, OUTPUT);
+	FastLED.addLeds<NEOPIXEL, ws2812b_pin>(leds, n_leds);
 	pinMode(13, OUTPUT);
 }
 
 void loop() {
-	digitalWrite(13, LOW);
-	delay(1000);
-	digitalWrite(13, HIGH);
-	delay(250);
-	digitalWrite(13, LOW);
-	delay(250);
-	digitalWrite(13, HIGH);
-	delay(250);
+	for (int i = 0; i < 256; i++){
+		for (int k = 0; k < n_leds; k++){
+			leds[k].r = 255 - i;
+			leds[k].g = i;
+		}
+		FastLED.show();
+	}
+	for (int i = 0; i < 256; i++){
+		for (int k = 0; k < n_leds; k++){
+			leds[k].g = 255 - i;
+			leds[k].b = i;
+		}
+		FastLED.show();
+	}
+	for (int i = 0; i < 256; i++){
+		for (int k = 0; k < n_leds; k++){
+			leds[k].b = 255 - i;
+			leds[k].r = i;
+		}
+		FastLED.show();
+	}
+
 }
